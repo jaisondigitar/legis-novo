ad2fe5bfeb9363efea71bf08243b9526ec344740
Marcelo Hoffmeister <marcelohenriquehoffmeister@gmail.com>
2021-11-12 08:39:26 -0400

    inserted assets

---
+++ public/assets/plugins/ckeditor/config.js
@@ -0,0 +1,15 @@
+/**
+ * @license Copyright (c) 2003-2015, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here. For example:
+	config.language = 'pt-br';
+    config.title=""
+    config.entities_latin = false;
+    config.enterMode = CKEDITOR.ENTER_BR;
+    config.allowedContent = true;
+};
+
+
