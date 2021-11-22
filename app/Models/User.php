b58c8fc01628c8a21293d9f86c93d27999a5a009
IgorM-Oliveira <igor.matos.oliveira.xyz@gmail.com>
2021-11-22 11:28:11 -0400

    fix validation

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,7 +48,7 @@ class User extends Model implements AuthenticatableContract, CanResetPasswordCon
     public static $rules = [
         'sector_id' => 'required',
         'name' => 'required',
-        'email' => 'required|unique:users,email',
+        'email' => 'exists:users,email',
         'password' => 'required',
     ];
 
