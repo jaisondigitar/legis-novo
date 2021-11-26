bb2b21703e9f39a9a99bedae06746e2820538c6c
Helton Varela <heltonapp@gmail.com>
2021-11-26 07:48:31 -0400

    remove fieldSearchable

--- app/Repositories/DocumentTypeRepository.php
+++ app/Repositories/DocumentTypeRepository.php
@@ -6,14 +6,5 @@ use App\Models\DocumentType;
 
 class DocumentTypeRepository extends Repository
 {
-    /**
-     * @var array
-     */
-    protected $fieldSearchable = [
-        'name',
-        'prefix',
-        'slug',
-    ];
-
     protected $modelClass = DocumentType::class;
 }
