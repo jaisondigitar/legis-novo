11ab756819547349e850c814f9278eea32e4bcbc
IgorM-Oliveira <igor.matos.oliveira.xyz@gmail.com>
2022-02-01 12:14:37 -0400

    add cdn font-awesome 5.15.4

--- resources/views/layouts/blit.blade.php
+++ resources/views/layouts/blit.blade.php
@@ -42,6 +42,7 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.42/css/bootstrap-datetimepicker.css" rel="stylesheet">
 
     <!-- MAIN CSS (REQUIRED ALL PAGE)-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="/assets/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet">
     <link href="/assets/css/style.css" rel="stylesheet">
     <link href="/assets/css/style-responsive.css" rel="stylesheet">
