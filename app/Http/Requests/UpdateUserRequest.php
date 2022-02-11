6376e7c6761aa1b0bc1516425684c6dcc96369cf
IgorM-Oliveira <igor.matos.oliveira.xyz@gmail.com>
2022-02-10 23:20:00 -0400

    user edit

--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -26,7 +26,7 @@ class UpdateUserRequest extends Request
             'email' => [
                 'nullable',
                 'string',
-                Rule::exists('users', 'email')->whereNull('deleted_at'),
+                Rule::unique('users', 'email')->whereNull('deleted_at'),
             ],
             'password' => 'nullable|string',
             'roles' => 'required|array',
