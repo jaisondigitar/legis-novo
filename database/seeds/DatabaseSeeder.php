f23bfd71074706450c08551156a8dbb9b2e27a1c
Marcelo Hoffmeister <marcelohenriquehoffmeister@gmail.com>
2022-02-14 22:04:31 -0400

    enable deactivated seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -20,8 +20,8 @@ class DatabaseSeeder extends Seeder
         $this->call(CompanyTableSeeder::class);
         $this->call(ModuleTableSeeder::class);
         $this->call(DocumentTypesTableSeeder::class);
-        //$this->call(StatesTableSeeder::class);
-        //$this->call(CitiesTableSeeder::class);
+        $this->call(StatesTableSeeder::class);
+        $this->call(CitiesTableSeeder::class);
         $this->call(DocumentProtocolTypeTableSeeder::class);
         $this->call(ParametersTableSeeder::class);
         $this->call(PlacesTypesTableSeeder::class);
