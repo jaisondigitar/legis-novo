4db9a379b86259e5811eced6d63af0e3af48d5a7
IgorM-Oliveira <igor.matos.oliveira.xyz@gmail.com>
2022-02-22 18:00:32 -0400

    add lib moment

--- resources/views/layouts/blit.blade.php
+++ resources/views/layouts/blit.blade.php
@@ -63,6 +63,8 @@
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.1/moment.min.js" integrity="sha512-qTXRIMyZIFb8iQcfjXWCO8+M5Tbc38Qi5WzdPOYZHIlZpzBHG3L3by84BBBOiRGiEb7KKtAOAs5qYdUiZiQNNQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
     <style>
         .pointer{
             cursor:pointer;
